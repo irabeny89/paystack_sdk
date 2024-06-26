c9635e7d570fec18e9c53a223439cdcab7ca8f2d
Irabeny <irabeny89@gmail.com>
2024-06-26 06:41:14 +0100

    chore: add amount conversion funtion to utils folder

---
+++ src/utils/amt_conv.ts
@@ -0,0 +1,31 @@
+/**
+ * Converts an amount to it's sub unit. This is useful because Paystack expects amounts to be in sub units for their supported currencies.
+ *
+ * eg:
+ * * `1` Nigerian Naira -> `100` Kobo
+ * * `1` Ghanian Cedi -> `100` Pesewas
+ * * `1` S.African Rand -> `100` Cent
+ *
+ * @export
+ * @param {number} amount - amount to convert
+ * @return {number} amount sub unit value
+ */
+export function convertToSubUnit(amount: number): number {
+  return amount * 100;
+}
+
+/**
+ * Converts a sub unit value to main unit value.
+ *
+ * eg:
+ * * `1` Nigerian Naira -> `100` Kobo
+ * * `1` Ghanian Cedi -> `100` Pesewas
+ * * `1` S.African Rand -> `100` Cent
+ *
+ * @export
+ * @param {number} amountInSubUnit - amount in sub unit
+ * @return {number} amount in main unit
+ */
+export function convertToMainUnit(amountInSubUnit: number): number {
+  return amountInSubUnit * 0.01;
+}
