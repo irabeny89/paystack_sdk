7ecaa5d6df96531c138e9f1ebc85baea85bc08b8
Irabeny <irabeny89@gmail.com>
2024-06-26 05:18:16 +0100

    chore(config): add config file

---
+++ config.ts
@@ -0,0 +1,28 @@
+export const PAYSTACK_BASE_URL = "https://api.paystack.co";
+
+export const TRANSACTION_INITIALIZE_PATH = "/transaction/initialize";
+
+export const TRANSACTION_VERIFY_PATH = "/transaction/verify/";
+
+export const TRANSACTION_LIST_PATH = "/transaction";
+
+export const TRANSACTION_CHARGE_AUTHORIZATION_PATH =
+  "/transaction/charge_authorization";
+
+export const TRANSACTION_TIMELINE_PATH = "/timeline/";
+
+export const TRANSACTION_TOTALS_PATH = "/transaction/totals";
+
+export const TRANSACTION_EXPORT_PATH = "/transaction/export";
+
+export const TRANSACTION_PARTIAL_DEBIT_PATH = "/transaction/partial_debit";
+
+export const TRANSFER_RECIPIENT_PATH = "/transferrecipient";
+
+export const TRANSFER_RECIPIENT_BULK_CREATE_PATH = "/transferrecipient/bulk";
+
+export const TRANSFER_PATH = "/transfer";
+
+export const TRANSFER_FINALIZE_PATH = "/transfer/finalize_transfer";
+
+export const TRANSFER_INITIATE_BULK_PATH = "/transfer/bulk";
