8b1a16eb5784e8637621a805ae6c983faa10823d
Irabeny <irabeny89@gmail.com>
2024-07-23 04:41:05 +0100

    docs(transaction): add vscode minimap labels

--- src/features/transaction.ts
+++ src/features/transaction.ts
@@ -56,9 +56,6 @@ import type {
  * - [x] Total amount received.
  * - [x] Export transaction records (currently as CSV file).
  * - [x] Partial debiting of customers.
- *
- * @export Transaction
- * @class Transaction
  */
 export class Transaction {
 	readonly logLevel: OptionT["logLevel"];
@@ -72,13 +69,13 @@ export class Transaction {
 	*/
 	readonly axiosPaystackClient: AxiosInstance;
 	
+	// #region constructor
 	/**
 	 * Debug levels are: `fatal`, `error`, `warn`, `info`, `debug`, `trace`, `silent`, `true`.
 	 *
 	 * This will stop at `trace` if set to `true` or `info` otherwise. Passing `silent` disables logging.
 	 *
 	 * @type {OptionT["logLevel"]}
-	 * @memberof Transaction
 	 */
 	constructor(paystackSecret: string, option?: OptionT) {
 		if (option?.logLevel) {
@@ -98,13 +95,13 @@ export class Transaction {
 		this.logger?.info("Transaction.constructor: created Axios instance");
 	}
 
+	// #region initialize
 	/**
 	 * # [Initialize Transaction](https://paystack.com/docs/api/transaction/#initialize)
 	 * Initialize a payment process and return data with authorization url for customer to complete payment, reference field to verify the payment on the backend later among other fields.
 	 *
 	 * @param {TransactionInitializeBodyParamsT} body request body.
-	 * @return data with reference field
-	 * @memberof Transaction
+	 * @return Axios response
 	 */
 	initialize(body: TransactionInitializeBodyParamsT) {
 		this.logger?.info(
@@ -118,13 +115,13 @@ export class Transaction {
 		>(TRANSACTION_INITIALIZE_PATH, body);
 	}
 
+	// #region verify
 	/**
 	 * # [Verify Transaction](https://paystack.com/docs/api/transaction/#verify)
 	 * Confirms the status of a transaction. Alternative is the `webhook` API on your backend that you register with Paystack on admin console where you listen for the `charge.success` event before giving value.
 	 *
 	 * @param {string} reference - a reference to an initialized/processed transaction
-	 * @return a response object
-	 * @memberof Transaction
+	 * @return Axios response
 	 */
 	verify(reference: string) {
 		this.logger?.info(
@@ -139,13 +136,13 @@ export class Transaction {
 		>(TRANSACTION_VERIFY_PATH + reference);
 	}
 
+	// #region list
 	/**
 	 * # [List Transactions](https://paystack.com/docs/api/transaction/#list)
 	 * List transactions carried out on your integration.
 	 *
 	 * @param {TransactionListQueryParamsT} [params] query parameters where defaults are - `perPage 50`, `page 1`
-	 * @return {*}  list of transactions
-	 * @memberof Transaction
+	 * @return Axios response
 	 */
 	list(params?: TransactionListQueryParamsT) {
 		this.logger?.info(
@@ -159,13 +156,13 @@ export class Transaction {
 		>(TRANSACTION_LIST_PATH, { params });
 	}
 
+	// #region fetch
 	/**
 	 * # [Fetch Transaction](https://paystack.com/docs/api/transaction/#fetch)
 	 * Get details of a transaction carried out on your integration.
 	 *
 	 * @param {string} transactionId id of transaction data to fetch
-	 * @returns a transaction data
-	 * @memberof Transaction
+	 * @returns Axios response
 	 */
 	fetch(transactionId: string) {
 		this.logger?.info(
@@ -180,13 +177,13 @@ export class Transaction {
 		>(`${TRANSACTION_LIST_PATH}/${transactionId}`);
 	}
 
+	// #region charge authorization
 	/**
 	 * # [Charge Authorization](https://paystack.com/docs/api/transaction/#charge-authorization)
 	 * All authorizations marked as reusable can be charged with this method whenever you need to receive payment without the sender entering their card details again.
 	 *
 	 * @param {TransactionChargeAuthorizationBodyParamsT} requestBody amount, email and authorization code are required
-	 * @return transaction response data
-	 * @memberof Transaction
+	 * @return Axios response
 	 */
 	chargeAuthorization(requestBody: TransactionChargeAuthorizationBodyParamsT) {
 		this.logger?.info(
@@ -200,13 +197,13 @@ export class Transaction {
 		>(TRANSACTION_CHARGE_AUTHORIZATION_PATH, requestBody);
 	}
 
+	// #region timeline
 	/**
 	 * # [Timeline of Transaction](https://paystack.com/docs/api/transaction/#view-timeline)
 	 * View the timeline of a transaction.
 	 *
 	 * @param {string} idOrReference id or reference of the transaction
-	 * @return response data
-	 * @memberof Transaction
+	 * @return Axios response
 	 */
 	timeline(idOrReference: string) {
 		this.logger?.info(
@@ -221,13 +218,13 @@ export class Transaction {
 		>(TRANSACTION_TIMELINE_PATH + idOrReference);
 	}
 
+	// #region totals
 	/**
 	 * # [Total Amount Received](https://paystack.com/docs/api/transaction/#totals)
 	 * Total amount received on your account.
 	 *
 	 * @param {TransactionTotalsQueryParamsT} [params] query parameters
-	 * @return response data
-	 * @memberof Transaction
+	 * @return Axios response
 	 */
 	totals(params?: TransactionTotalsQueryParamsT) {
 		this.logger?.info(
@@ -241,13 +238,13 @@ export class Transaction {
 		>(TRANSACTION_TOTALS_PATH, { params });
 	}
 
+	// #region export
 	/**
 	 * # [Export Transaction Records](https://paystack.com/docs/api/transaction/#export)
 	 * Export list of transaction records carried out on your integration. Mostly a path to download as a Comma Separated Value (CSV) file.
 	 *
 	 * @param {TransactionExportParamsT} [params] optional query parameters
-	 * @return response data
-	 * @memberof Transaction
+	 * @return Axios response
 	 */
 	export(params?: TransactionExportParamsT) {
 		this.logger?.info(
@@ -261,13 +258,13 @@ export class Transaction {
 		>(TRANSACTION_EXPORT_PATH, { params });
 	}
 
+	// #region partial debit
 	/**
 	 * # [Partial Debit](https://paystack.com/docs/api/transaction/#partial-debit)
 	 * Debit customer partially.
 	 *
 	 * @param {TransactionPartialDebitBodyParamsT} requestBody request body
-	 * @return transaction response
-	 * @memberof Transaction
+	 * @return Axios response
 	 */
 	partialDebit(requestBody: TransactionPartialDebitBodyParamsT) {
 		this.logger?.info(
