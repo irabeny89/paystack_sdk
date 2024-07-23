bbbc9352ddd08e13cffe99e415c48f7ba562d28c
Irabeny <irabeny89@gmail.com>
2024-07-23 03:07:34 +0100

    style(transfer-recipient): update recipient option types

--- src/types/transfer_recipient_types.d.ts
+++ src/types/transfer_recipient_types.d.ts
@@ -3,7 +3,7 @@ import {
 	DomainOptionT,
 	IdAndTimestampsT,
 	MetaDataT,
-	RecipientTypeT,
+	RecipientOptionT,
 } from "./global";
 
 type TransferRecipientDetailT = {
@@ -18,7 +18,7 @@ export type TransferRecipientNubanOrBasaBodyParamsT = {
 	/** recipient type: `nuban`, `basa`, or `mobile_money`.
 	 * `nuban` for Nigeria, `mobile_money` for Ghana and `basa` for S.Africa.
 	 */
-	type: Omit<RecipientTypeT, "mobile_money">;
+	type: Omit<RecipientOptionT, "mobile_money">;
 	/** required if type is `nuban` or `basa`. You can get bank list from the `List Banks` API on Paystack docs */
 	bank_code: string;
 	/** required if type is `nuban` or `basa` */
@@ -41,10 +41,10 @@ export type TransferRecipientMobileMoneyBodyParamsT = Omit<
 > &
 	Record<"type", "mobile_money">;
 
-export type GetTransferRecipientBodyParamsT<T extends RecipientTypeT> =
+export type GetTransferRecipientBodyParamsT<T extends RecipientOptionT> =
 	T extends "mobile_money"
-		? TransferRecipientMobileMoneyBodyParamsT
-		: TransferRecipientNubanOrBasaBodyParamsT;
+	? TransferRecipientMobileMoneyBodyParamsT
+	: TransferRecipientNubanOrBasaBodyParamsT;
 
 export type TransferRecipientBulkCreateBodyParamsT = {
 	/** list of transfer recipient object. Each object should contain required fields for recipient type(`nuban`, `basa` or `mobile_money`) option.
@@ -60,7 +60,7 @@ export type TransferRecipientResponseDataT = {
 	integration: number;
 	name: string;
 	recipient_code: string;
-	type: RecipientTypeT;
+	type: RecipientOptionT;
 	active: boolean;
 	currency: CurrencyOptionT;
 	domain: DomainOptionT;
