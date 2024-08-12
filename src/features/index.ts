008c56e9a7580c753553ebfb7cfce0f7498d11f2
irabeny89 <58922232+irabeny89@users.noreply.github.com>
2024-08-12 23:15:52 +0000

    style: format and lint

--- src/features/index.ts
+++ src/features/index.ts
@@ -3,4 +3,4 @@ export { Transfer } from "./transfer";
 export { TransferRecipient } from "./transferRecipient";
 export { Plan } from "./plans";
 export { Subscription } from "./subscription";
-export { Verification } from "./verification"
+export { Verification } from "./verification";
