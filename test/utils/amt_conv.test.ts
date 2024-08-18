d4d9f4e39480018644bca17daf2a32842c10a07a
Irabeny <irabeny89@gmail.com>
2024-08-18 23:13:11 +0100

    fix(amt_conv.ts): handle amount values convertToMain and convertToSubUnit functions with test

--- test/utils/amt_conv.test.ts
+++ test/utils/amt_conv.test.ts
@@ -5,10 +5,18 @@ describe("convertToMainUnit", () => {
 	test("should convert amount in sub unit to main unit", () => {
 		expect(convertToMainUnit(100)).toBe(1);
 	});
+
+	test("should error if amount has decimal values", () => {
+		expect(() => convertToMainUnit(100.9)).toThrowError();
+	});
 });
 
 describe("convertToSubUnit", () => {
 	test("should convert amount in main unit to sub unit", () => {
 		expect(convertToSubUnit(1)).toBe(100);
+	});
+
+	test("should error if amount has more than 2 decimal values", () => {
+		expect(() => convertToSubUnit(100.555)).toThrowError();
 	});
 });
