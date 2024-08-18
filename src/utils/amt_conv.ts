2007b099aa05c001b83c08991b7a853e28790d84
Irabeny <irabeny89@gmail.com>
2024-08-18 23:13:11 +0100

    fix(amt_conv.ts): handle amount values convertToMain and convertToSubUnit functions with test

--- src/utils/amt_conv.ts
+++ src/utils/amt_conv.ts
@@ -5,17 +5,25 @@
  * * `1` Nigerian Naira -> `100` Kobo
  * * `1` Ghanian Cedi -> `100` Pesewas
  * * `1` S.African Rand -> `100` Cent
+ * * `1` US Dollar -> `100` Cent
  *
  * @export
- * @param {number} amount - amount to convert
- * @return {number} amount sub unit value
+ * @param amount - amount to convert with no or at most 2 decimal values
+ * @return amount sub unit value
  *
  * @example
  * ```ts
- * 	const kobo = convertToSubUnit(1) // 100
+ *  const amount = 1
+ * 	const kobo = convertToSubUnit(amount) // 100
  * ```
  */
 export function convertToSubUnit(amount: number): number {
+	const [, decimalPart] = amount.toString().split(".");
+	if (decimalPart?.length > 2)
+		throw new Error(
+			`"amount" value should not have more than 2 decimal values`,
+		);
+	// ? remove decimal to maintain sub unit value
 	return amount * 100;
 }
 
@@ -26,16 +34,21 @@ export function convertToSubUnit(amount: number): number {
  * * `1` Nigerian Naira -> `100` Kobo
  * * `1` Ghanian Cedi -> `100` Pesewas
  * * `1` S.African Rand -> `100` Cent
+ * * `1` US Dollar -> `100` Cent
  *
  * @export
- * @param {number} amountInSubUnit - amount in sub unit
- * @return {number} amount in main unit
+ * @param amount - amount in sub unit with no decimal values
+ * @return amount in main unit
  *
  * @example
  * ```ts
- * 	const naira = convertToSubUnit(100) // 1
+ *  const amount = 100
+ * 	const naira = convertToSubUnit(amount) // 1
  * ```
  */
-export function convertToMainUnit(amountInSubUnit: number): number {
-	return amountInSubUnit * 0.01;
+export function convertToMainUnit(amount: number): number {
+	// ! zero value decimals (eg .00, 100.00 == 100) will be ignored
+	if (amount.toString().includes("."))
+		throw new Error(`"amount" value should not have decimal values`);
+	return amount * 0.01;
 }
